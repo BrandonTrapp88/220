0680f52ec1292acd1035c061b661ae62484ac0b7
brandon <trappbm@g.cofc.edu>
2022-01-19 11:03:05 -0500

    Homework 1 completeted

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,73 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Brandon Trapp
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: 5 different functions that uses input() to figure out different problems.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
 def calc_rec_area():
-    pass
+    #ask user for the different mesurements
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    # area = length muliplied by the width
+    area = length * width
+    #post answer for the user
+    print("Area = ",area)
 
 
 def calc_volume():
-    pass
+    #ask user for the different mesurements
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    #calulation for the volume
+    volume = length * width * height
+    #post volume for the user
+    print("The volume is ", volume)
+
 
 
 def shooting_percentage():
-    pass
+    #ask user for the for the total shots taking and the total shots made
+    total_Shots = eval(input("Enter the player total shots: "))
+    shots_Made = eval(input("Enter how many shots the player made: "))
+    #calculate the percentage and muliply that by 100 to covert the decimal to percetage.
+    percentage = shots_Made / total_Shots
+    new_Percentage = percentage * 100
+    #display user shooting percentage
+    print("Shooting percentage: ",new_Percentage,"%")
+
+
 
 
 def coffee():
-    pass
+    #ask user for how many pounds of coffee
+    pounds_coffee = eval(input("How many pounds of coffee do you want: "))
+    #calculating the total cost of coffee
+    coffee_cost = 10.50 * pounds_coffee
+    shipping_cost = 0.86 * pounds_coffee
+    fixed_cost = 1.50
+    total_cost = coffee_cost + shipping_cost + fixed_cost
+    #display total cost for user
+    print("Your total is : $", total_cost)
+
 
 
 def kilometers_to_miles():
-    pass
+    #ask user for how many kilometers they drove
+    kilometers_Driven = eval(input("How many kilometers did you drive: "))
+    #covert kilometers to miles
+    covert = 1.61
+    miles = kilometers_Driven / covert
+    #display to miles to user
+    print("you drove",miles, "miles ")
+
+
 
 
 if __name__ == '__main__':
