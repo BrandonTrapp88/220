a00a46a3d0fa2b4d26ea1f48df6fb8b148409569
brandon <trappbm@g.cofc.edu>
2022-01-26 10:50:49 -0500

    homework 2 completed

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,36 +1,72 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Brandon Tra--
+hw2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: creating different functions using import math
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    #setting a variable of sum  to 0. This stores my numbes in my for loop
+    sum = 0
+    #gets the upper bound number from user.
+    upper_bound = eval(input("Enter the upper bound number: "))
+    #loop that goes through the upperbound, starting at the number 3 by 3's
+    for i in range(3,upper_bound+1,3):
+        sum = i+sum
+    print("sum of threes is: ", sum)
+
 
 
 def multiplication_table():
-    pass
+    #using a for loop to print out the table
+    for i in range(1,11):
+        print()
+        for j in range(1,11):
+            #multiplying the numbers to get the other totals
+            lines = i * j
+            print(lines, end=" \t")
+
+
 
 
 def triangle_area():
-    pass
+    #getting the messurements for the sides of the triangle
+    side_a = eval(input("Enter a value for side a: "))
+    side_b = eval(input("Enter a value for side b: "))
+    side_c = eval(input("Enter a value for side c: "))
+    #the formula for getting the area of a triangle
+    square_s = (side_a+side_b+side_c)/2
+    total_square = (square_s*((square_s-side_a)*(square_s-side_b)*(square_s-side_c)))
+    print("The area of your triangle is ", math.sqrt(total_square))
 
 
 def sum_squares():
-    pass
+    #getting the numbers from user to square
+    lower_range = eval(input("Enter the lower range: "))
+    upper_range = eval(input("Enter the upper range: "))
+    sum = 0
+    #going through the variables storing the answer in the sum variable
+    for i in range(lower_range, upper_range+1):
+        sum = (i **2) + sum
+    print(sum)
+
 
 
 def power():
-    pass
-
+    #getting the base and exponent from the user.
+    base = eval(input("Enter the base: "))
+    exponent = eval(input("Enter the exponent: "))
+    #creating the answer
+    answer = base ** exponent
+    #displaying the answer
+    print(base,"^", exponent, " = ", answer)
 
 if __name__ == '__main__':
     pass
