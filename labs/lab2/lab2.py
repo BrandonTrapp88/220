bd3d1cd6eaa66e54f4abf171d35d57ea29a7cc12
brandon <trappbm@g.cofc.edu>
2022-01-27 21:12:59 -0500

    Lab 2 completed

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -10,6 +10,8 @@ I certify that this assignment is entirely my own work.
 
 """
 import math
+
+
 def means():
     #accumlator vars
     rms = 0
@@ -20,12 +22,12 @@ def means():
     #for loop that goes through the number of submissions storing the values
     for i in range(number_of_submissions):
         numbers = eval(input("Enter a number: "))
-        rms = (numbers**2) + rms
-        harmonic_mean = (1/numbers)+ harmonic_mean
+        rms = (numbers ** 2) + rms
+        harmonic_mean = (1 / numbers) + harmonic_mean
         geometric_mean = numbers * geometric_mean
-    root_mean_square_answer = math.sqrt(rms/number_of_submissions)
+    root_mean_square_answer = math.sqrt(rms / number_of_submissions)
     harmonic_mean_answer = number_of_submissions / harmonic_mean
-    geometric_mean_answer = geometric_mean ** (1/number_of_submissions)
+    geometric_mean_answer = geometric_mean ** (1 / number_of_submissions)
     #display to the user their answer
     print("Your Root-Mean-Square answer is: ", root_mean_square_answer)
     print("Your harmonic mean answer is: ", harmonic_mean_answer)
