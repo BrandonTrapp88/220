f112ac0dc4551fcc5a1bd185dcfa34fc71162171
brandon <trappbm@g.cofc.edu>
2022-02-18 00:19:30 -0500

    hw5 completed

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,42 +1,101 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Brandon Trapp
+hw5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Practice using different string methods
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def name_reverse():
-    pass
 
+    name = input("What is your name: ")
+    name = name.split(" ")
+    first = name[0]
+    last = name[1]
+    print(last+", "+first)
 
 def company_name():
-    pass
 
+    name = input("Enter company name: ")
+    name = name.split(".")
+    comp_name = name[1]
+    print(comp_name)
 
 def initials():
-    pass
+
+
+    numb = eval(input("How many student names will you like to enter? "))
+
+
+    for i in range(numb):
+       names = input("Enter student name ")
+       names = names.split(" ")
+       first = str(names[0])
+       last = str(names[1])
+       print(first[0] + last[0])
 
 
 def names():
-    pass
 
+    name = input("Enter a list of names: ")
+    initals  = name.split(",")
+    first = " "
+
+    for i in initals:
+        first = i.split()
+        first_name = first[0]
+        last_name = first[1]
+        answer = first_name[0]+last_name[0]
+        print(answer, end=" ")
 
 def thirds():
-    pass
 
+    sent = eval(input("Enter number of sentences: "))
+    blank_list =[]
+
+    for i in range(sent):
+        question = input("Enter your sentence: ")
+        answer = question[0::3]
+        blank_list.append(answer)
+    for j in blank_list:
+        print(j)
 
 def word_average():
-    pass
+    sent = input("Enter a sentece: ")
+    happy = sent.split()
+    num = len(happy)
+    word_count = 0
+    for i in happy:
+       word_count = word_count + len(i)
+    print(word_count/num)
 
 
 def pig_latin():
-    pass
+
+    sent = input("Enter a sentence: ")
+    sent = sent.split()
+    h1 = " "
+    answer = " "
+
+
+
+
+    for i in sent:
+        letter = i[0]
+        hi = h1+i[1:: ]
+
+        answer =answer + ((hi.lower() + letter.lower() + "ay" ))
+        answer= answer.lstrip()
+    print(answer)
+
+
+
+
+
 
 
 if __name__ == '__main__':
