b5dc45b93ef73dc1fdaa66230191b11b2e4a3fcb
brandon <trappbm@g.cofc.edu>
2022-03-24 21:18:05 -0400

    lab9

--- labs/lab9/lab9.py
+++ labs/lab9/lab9.py
@@ -1,12 +1,16 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Brandon Trapp
+lab9.py
+
+I certify that this assignment is entirely my own work.
+
 """
 
 
 def build_board():
-    pass
 
+    number_list = [1,2,3,4,5,6,7,8,9]
+    return number_list
 
 def print_board(board):
     """ prints the values of baord """
@@ -35,33 +39,67 @@ def print_board(board):
     print(row_3)
     print(reset)
 
-
 def is_legal(board, position):
-    pass
 
+    if str(board[position-1]).isnumeric():
+        return True
+    else:
+        return False
 
 def fill_spot(board, position, character):
-    pass
+    character = character.strip()
 
+    board[position - 1] = character.lower()
 
 def winning_game(board):
-    pass
 
+    if board[0] == board[1] == board[2]:
+        return True
+    elif board[2]==board[4]==board[6]:
+        return True
+    elif board[6]==board[7]==board[8]:
+        return True
+    elif board[2]==board[5]==board[8]:
+        return True
+    elif board[0]==board[3]==board[6]:
+        return True
+    elif board[0]==board[4]==board[8]:
+        return True
+    else:
+        return False
 
 def game_over(board):
-    pass
 
+    game = winning_game(board)
+    if game == True:
+        return True
+    else:
+        return False
 
 def get_winner(board):
     pass
 
-
 def play(board):
-    pass
 
+   print("Enter a number on the board to put an x or o into the spot. ")
+   play = "y" or "Y"
+   while play == "y" or "Y":
+       print_board(board)
+       while game_over(board) == False:
+           player1 = eval(input("Player x, pick a spot with a number: "))
+           if is_legal(board, player1) == True:
+               fill_spot(board,player1,'x')
+           else:
+               print("Pick again")
+
+           player2 = eval(input("Player o, pick a spot with a number"))
+           if is_legal(board, player2) == True:
+               fill_spot(board, player2, "o")
+           else:
+               print("pick again")
 
 def main():
-    pass
+    play(build_board())
 
 
 if __name__ == '__main__':
